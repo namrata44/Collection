bdaebf922e401ef9b99baa82375ebfff468b47fa
namrata44 <103646973+namrata44@users.noreply.github.com>
2023-01-27 21:12:57 +0530

    initial commit

---
+++ src/com/arraylist/Example.java
@@ -0,0 +1,19 @@
+package com.arraylist;
+
+import java.util.ArrayList;
+
+public class Example {
+	
+	public static void main(String[]args) {
+		
+		ArrayList<String> Name=new ArrayList<String>();
+		Name.add("Nikita");
+		Name.add("Isha");
+		Name.add("Richa");
+		
+		System.out.println(Name);
+		Name.add("Aradhana");
+		System.out.println(Name);
+	}
+
+}
