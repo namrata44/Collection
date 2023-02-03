aee4aee96469143b57db3c4b8b130cbc501e9e2d
namrata44 <103646973+namrata44@users.noreply.github.com>
2023-02-03 19:38:37 +0530

    Exception programs

---
+++ dailywork/Exception/src/com/exception/Exception.java
@@ -0,0 +1,21 @@
+package com.exception;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		
+		try {
+
+		int a = 100, b = 0, c;
+		c = a / b;
+
+		System.out.println(c);
+		}
+		
+		catch(ArithmeticException e) {
+			System.out.println(e);
+			System.out.println("you cannot divided by zero");
+		}
+	}
+
+}
--- dailywork/Exception/src/com/exception/HelloException.java
+++ dailywork/Exception/src/com/exception/HelloException.java
@@ -1,33 +1,28 @@
 package com.exception;
+
 import java.util.Scanner;
+
 @SuppressWarnings("serial")
-class HelloException extends Exception 
-{
-	Scanner  scan = new Scanner(System.in);
+class HelloException extends Exception {
+	Scanner scan = new Scanner(System.in);
 	String str;
-	void check()
-	{
-		try
-		{
+
+	void check() {
+		try {
 			System.out.println("Enter the string:");
 			str = scan.next();
-			if(str.equals("hello"))
-			{
+			if (str.equals("hello")) {
 				throw new HelloException();
-			}
-			else
-			{
+			} else {
 				System.out.println("There is no Exception!");
 			}
-		}catch(HelloException e)
-		{
+		} catch (HelloException e) {
 			System.out.println(e);
 			System.out.println("There is an Exception !");
 		}
 	}
-	public String toString()
-	{
+
+	public String toString() {
 		return "User Input is hello";
 	}
 }
-
